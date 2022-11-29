c396a7026e45e8093d65398125530d2f91a3d2da
andy <andy.rosete@novafori.com>
2022-11-29 23:02:10 +0100

    fix code error

--- src/layouts/app.layout.tsx
+++ src/layouts/app.layout.tsx
@@ -3,7 +3,11 @@ import { AppBar, Toolbar, IconButton } from '@mui/material';
 import * as React from 'react';
 import * as classes from './app.layout.styles';
 
-export const AppLayout: React.FunctionComponent = (props) => {
+export interface Props {
+  children?: React.ReactNode;
+}
+
+export const AppLayout: React.FunctionComponent<Props> = (props: any) => {
   const { children } = props;
 
   return (
